497ba0b8055cf180bf2bba6a0aba5ce13a132b6c
coelhodiana <coelhodiana04@gmail.com>
2023-06-15 04:59:52 -0400

    chore: configurando firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { firebaseProviders } from 'src/environments/environments';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+  providers: [provideRouter(routes), firebaseProviders],
 };
